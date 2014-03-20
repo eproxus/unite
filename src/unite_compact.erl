507402cf26db37364630272accf6d44476c2b9e8
Adam Lindberg <hello@alind.io>
2014-03-20 12:19:13 +0100

    Remove debug output

--- src/unite_compact.erl
+++ src/unite_compact.erl
@@ -68,7 +68,7 @@ print_failures(Failures) ->
     [print_failure(F) || F <- Failures].
 
 print_failure(Failure) ->
-    io:format("~p~n~n", [Failure]),
+    % io:format("~p~n~n", [Failure]),
     {Type, Info, Case} = failure_info(Failure),
     io:format("~s~n", [
         color:redb([format_type(Type),
