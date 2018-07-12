cded7b883b16e72fd08e4f6257e50001bf6f0e53
Adam Lindberg <hello@alind.io>
2018-07-12 11:15:47 +0200

    Update release preparation helpers

---
+++ .scripts/cut.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+set -e # Abort on first failure, so we don't mess something up
+
+if [ -z "$1" ]; then
+    # Missing tag name
+    echo "usage: cut <version>" >&2
+    exit 129
+fi
+if [ ! -z "$(git status --short)" ]; then
+    # Sanity check
+    echo "fatal: dirty repository" >&2
+    exit 128
+fi
+
+VSN="$1"
+
+# Update version
+sed -i "" -e "s/{vsn, .*}/{vsn, \"$VSN\"}/g" src/*.app.src
+git add src/*.app.src
+if [ -f doc/overview.edoc ]; then
+    sed -i "" -e "s/@version .*/@version $VSN/g" doc/overview.edoc
+    git add doc/overview.edoc
+fi
+
+# Commit, tag and push
+git commit -m "Version $VSN"
+git tag -s "$VSN" -m "Version $VSN"
+git push && git push --tags
+
+# Clean and publish package and docs
+rm -rf ebin
+rm -rf src/**/*.beam
+rm -rf test/**/*.beam
+rebar3 hex publish
+#rebar3 hex docs
+
+# Generate and push changelog
+github_changelog_generator
+git add CHANGELOG.md
+git commit -m "Update Changelog for version $VSN"
+git push
